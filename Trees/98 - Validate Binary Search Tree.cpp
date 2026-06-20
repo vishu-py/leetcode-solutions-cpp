ad48a90b7ef1625c3337571b0528384abc0bb6a4
vishu-py <er.vaishnavi7@gmail.com>
2026-06-20 19:36:28 +0530

    Add to Trees solution

---
+++ Trees/98 - Validate Binary Search Tree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* root, long minVal, long maxVal){
+        if(root == NULL) return true;
+
+        if(root->val <= minVal || root->val >= maxVal){
+            return false;
+        }
+
+        return solve(root->left, minVal, root->val) &&
+               solve(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return solve(root, LONG_MIN, LONG_MAX);
+    }
+};
