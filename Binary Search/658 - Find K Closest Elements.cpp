4a89ec0d4a9cfa02d197b9edaa75d93b1b31b5e8
vishu-py <er.vaishnavi7@gmail.com>
2026-07-01 22:08:03 +0530

    Add to Binary Search solution

---
+++ Binary Search/658 - Find K Closest Elements.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int left =0;
+        int right = n-k;
+        while(left<right){
+            int mid = left + (right - left)/2;
+
+            if(x-arr[mid] > arr[mid+k]-x){
+                left=mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return vector<int>(arr.begin()+left , arr.begin()+left+k);
+    }
+};
