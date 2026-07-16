16e7319ee4c54365c209b663a78c89b3ee88cae5
vishu-py <er.vaishnavi7@gmail.com>
2026-07-16 13:19:09 +0530

    Add to Recursion solution

---
+++ Recursion/24 - Swap Nodes in Pairs.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* first=head;
+        ListNode* second=first->next;
+
+        first->next=swapPairs(second->next);
+
+        second->next=first;
+        return second;
+    }
+};
