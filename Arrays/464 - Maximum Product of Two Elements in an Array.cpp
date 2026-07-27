0da7637cf33a28db3662daf19d493b38e3a3b784
vishu-py <er.vaishnavi7@gmail.com>
2026-07-27 23:38:41 +0530

    Add to Arrays solution

---
+++ Arrays/464 - Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max=0;
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(max<(nums[i]-1)*(nums[j]-1) && i!=j){
+                    max=(nums[i]-1)*(nums[j]-1);
+                }
+            }
+        }
+
+        return max;
+    }
+};
