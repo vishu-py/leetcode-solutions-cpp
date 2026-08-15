c4d6c50296db699f7f8fb77745e7f5af19fcaff0
vishu-py <er.vaishnavi7@gmail.com>
2026-08-15 23:29:49 +0530

    Add to Dynamic Programming solution

---
+++ Dynamic Programming/70 - Climbing Stairs.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        
+        int prev1=1;
+        int prev2=2;
+        int curr=0;
+
+        for(int i=3; i<=n; i++){
+            curr=prev1+prev2;
+
+            prev1=prev2;
+            prev2=curr;
+        }
+
+        return curr;
+    }
+};
