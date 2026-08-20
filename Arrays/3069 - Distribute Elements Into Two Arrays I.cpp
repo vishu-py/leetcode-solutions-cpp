64b2de0db198f9dfaaa96d9e571a09583ee8b91d
vishu-py <er.vaishnavi7@gmail.com>
2026-08-20 22:48:19 +0530

    Add to Arrays solution

---
+++ Arrays/3069 - Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        vector<int>arr1;
+        vector<int>arr2;
+            arr1.push_back(nums[0]);
+            arr2.push_back(nums[1]);
+            
+        for(int i =2 ; i<n;i++){
+            if(arr1.back() > arr2.back()){
+                arr1.push_back(nums[i]);
+            }else{
+                arr2.push_back(nums[i]);
+            } 
+        }
+        for(int x : arr1){
+            ans.push_back(x);
+        }
+        for(int x : arr2){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
