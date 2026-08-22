05dd95dcb06d1b5707428f72c1499986eff1434c
vishu-py <er.vaishnavi7@gmail.com>
2026-08-22 19:05:45 +0530

    Add to Hashing solution

---
+++ Hashing/525 - Contiguous Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        mp[0]=-1;
+        int sum=0;
+        int ans=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1){
+                sum++;
+            }
+            else{
+                sum--;
+            }
+
+            if(mp.count(sum)){
+                ans=max(ans, i-mp[sum]);
+            }
+            else{
+                mp[sum]=i;
+            }
+        }
+
+        return ans;
+    }
+};
