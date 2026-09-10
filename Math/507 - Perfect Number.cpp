40f0588dafa6d84def62c440c652aa0cdb0e4e99
vishu-py <er.vaishnavi7@gmail.com>
2026-09-10 22:28:10 +0530

    Add to Math solution

---
+++ Math/507 - Perfect Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1; i<num; i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+
+        if(sum==num){
+            return true;
+        }
+        
+        return false;
+    }
+};
