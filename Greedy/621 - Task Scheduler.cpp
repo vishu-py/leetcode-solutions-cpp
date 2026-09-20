11a6d1fe529e40114f020b46942c6d90fd931661
vishu-py <er.vaishnavi7@gmail.com>
2026-09-20 22:54:37 +0530

    Add to Greedy solution

---
+++ Greedy/621 - Task Scheduler.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26, 0);
+
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        int maxFreq = *max_element(freq.begin(), freq.end());
+
+        int countMax = 0;
+        for (int f : freq) {
+            if (f == maxFreq)
+                countMax++;
+        }
+
+        int ans = (maxFreq - 1) * (n + 1) + countMax;
+
+        return max((int)tasks.size(), ans);
+    }
+};
