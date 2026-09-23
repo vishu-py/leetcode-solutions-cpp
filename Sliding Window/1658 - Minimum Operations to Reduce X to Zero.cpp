3c85d90d4de6d7c8ec17fa356a1fa7e4434cf201
vishu-py <er.vaishnavi7@gmail.com>
2026-09-23 22:47:33 +0530

    Add to Sliding Window solution

---
+++ Sliding Window/1658 - Minimum Operations to Reduce X to Zero.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+
+        int total = 0;
+        for (int num : nums)
+            total += num;
+
+        int target = total - x;
+
+        if (target < 0)
+            return -1;
+
+        if (target == 0)
+            return n;
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1)
+            return -1;
+
+        return n - maxLen;
+    }
+};
